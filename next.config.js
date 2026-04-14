5152a1942e4b43e6418b092313acab03777a260e
Your Name <your@email.com>
2026-04-15 00:23:52 +0600

    Auto-save: update project files

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    quality: 90,
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "randomuser.me" },
