2a443112bbc2d396de1204c85ba05a66943a50cd
Your Name <your@email.com>
2026-04-20 06:03:45 +0600

    fix: homepage ad banner height 90px same as article banner

--- components/sections/InlineAdBanner.tsx
+++ components/sections/InlineAdBanner.tsx
@@ -31,7 +31,7 @@ export default function InlineAdBanner({ placement = "Between Article Paragraphs
       <a href={ad.linkUrl || "#"} target="_blank" rel="noopener noreferrer"
         className="block w-full overflow-hidden border-y border-brand-border hover:opacity-95 transition-opacity">
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src={ad.imageUrl} alt={ad.name} className={`w-full block object-cover ${fullSize ? "h-[120px]" : "h-[90px]"}`} />
+        <img src={ad.imageUrl} alt={ad.name} className="w-full block object-cover h-[90px]" />
       </a>
     );
   }
