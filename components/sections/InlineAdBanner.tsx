c5f7f4775b1eed56da2b07a51cf306cb0c900879
Your Name <your@email.com>
2026-04-20 06:31:42 +0600

    Revert "fix: ad banner auto height shows full image"
    This reverts commit 2a3eb5217d56a39a33c95a40a2d70f8988fac643.

--- components/sections/InlineAdBanner.tsx
+++ components/sections/InlineAdBanner.tsx
@@ -31,7 +31,7 @@ export default function InlineAdBanner({ placement = "Between Article Paragraphs
       <a href={ad.linkUrl || "#"} target="_blank" rel="noopener noreferrer"
         className="block w-full overflow-hidden border-y border-brand-border hover:opacity-95 transition-opacity">
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src={ad.imageUrl} alt={ad.name} className="w-full block h-auto" />
+        <img src={ad.imageUrl} alt={ad.name} className="w-full block object-cover h-[140px]" />
       </a>
     );
   }
